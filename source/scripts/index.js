bd46264ee75394e32ada1645b85ff40776fa73e2
herold89 <heroldserg89@gmail.com>
2025-09-22 08:19:18 +0300

    добавляет разметку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.header__mobile-menu');
+const headerLogo = document.querySelector('.header__logo');
+const userMenu = document.querySelector('.user-menu');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger--active');
+  headerLogo.classList.toggle('header__logo--hidden');
+  userMenu.classList.toggle('user-menu--active');
+  if (mobileMenu.open) {
+    mobileMenu.close();
+  } else {
+    mobileMenu.show();
+  }
+});
